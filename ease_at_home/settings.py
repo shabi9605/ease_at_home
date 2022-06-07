6e587ef371c1862c14df0032a2991cde9760de53
shabi9605 <shabeebshabi960580@gmail.com>
2022-06-07 22:24:35 +0530

    hi

--- ease_at_home/settings.py
+++ ease_at_home/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
+django_heroku.settings(locals())
 
 import os
 
