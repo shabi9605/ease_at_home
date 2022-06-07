814a92caa6bb206652edd7db84fe8760b12cb129
shabi9605 <shabeebshabi960580@gmail.com>
2022-06-07 23:42:10 +0530

    code updated

--- ease_at_home/settings.py
+++ ease_at_home/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,7 +139,7 @@ if DEBUG:
 else:
    STATIC_ROOT
 
-django_heroku.settings(locals())
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
