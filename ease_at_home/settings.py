b641544c7cc9838b61f29fa4d19d4d0802f80b9a
shabi9605 <shabeebshabi960580@gmail.com>
2022-06-08 00:42:00 +0530

    customer dashboard upgraded to responsive

--- ease_at_home/settings.py
+++ ease_at_home/settings.py
@@ -127,17 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # STATICFILES_ROOT = [
 #     STATIC_ROOT,
 # ]
 
 if DEBUG:
    STATICFILES_DIRS = [
-   STATIC_DIRS,
+   STATIC_ROOT,
    ]
 else:
-   STATIC_DIRS
+   STATIC_ROOT
 
 
 
